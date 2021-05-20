e2052de4c8010d2d8f7df6bff877281b85564c17
filipemascarenhaspaiva <83734487+filipemascarenhaspaiva@users.noreply.github.com>
2021-05-20 07:44:25 -0300

    Delete Aula 9 directory

--- Aula 9/Aula_9_Exercicio_1_For.py
+++
@@ -1,7 +0,0 @@
-'''Crie um código em Python que pede qual tabuada o usuário 
-quer ver, em seguida imprima essa tabuada.'''
-
-num1 = int(input('VocÊ quer saber a tabuada de que numero?\n'))
-
-for num2 in range(1, 10+1):
-    print(num1, 'x', num2, '=', num1*num2)
